66f2a3bbbd9d9b5e397de2561474c7c42aaa49e7
AsZaychik <aszaychik@gmail.com>
2023-05-07 17:55:23 +0700

    try routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
+import routes from './routes/index.js';
 
 const app: Express = express();
 const port: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`⚡：[server]: Server is running at https://localhost:${port}`);
