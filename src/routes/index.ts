13cd5282eb10aff26981e97fca5445e75c030212
AsZaychik <aszaychik@gmail.com>
2023-05-07 18:31:06 +0700

    request url

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,4 +6,17 @@ router.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+router.get('/hello/world', (req: Request, res: Response) => {
+  res.json({
+    path: req.path,
+    originalUrl: req.originalUrl,
+    method: req.method,
+    hostname: req.hostname,
+    protocol: req.protocol,
+    message: 'Hello World!',
+    timestamp: new Date().toISOString(),
+    success: true,
+  });
+});
+
 export default router;
